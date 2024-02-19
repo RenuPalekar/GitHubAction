447d51ba01504cf9053ebfcce1a9371ab0c39d59
RenuPalekar <159442231+RenuPalekar@users.noreply.github.com>
2024-02-19 10:37:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello python")
+import sys
+ 
+def main(url, output_file):
+    # Simple message indicating the parameters
+    print(f"Downloading file from {url} and saving it to {output_file}")
+ 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    output_file = sys.argv[2]
+    main(url, output_file)
